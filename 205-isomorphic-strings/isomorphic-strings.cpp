fec454078178258696260e6ac9a95bdb35b00a57
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-02-11 01:09:19 +0500

    Time: 3 ms (42.41%) | Memory: 9.4 MB (54.05%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size()) return false;
+
+        unordered_map<char,int> smap , tmap;
+
+        for(int i=0; i<s.size(); i++){
+            if(smap.find(s[i]) == smap.end()){
+                smap[s[i]] = i;
+            }
+
+            if(tmap.find(t[i]) == tmap.end()){
+                tmap[t[i]] = i;
+            }
+
+            if(smap[s[i]] != tmap[t[i]]) return false;
+        }
+
+        return true;
+    }
+};
