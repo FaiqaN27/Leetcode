9d2df39e31f3a9ebafa9b050faf0481bb2ed97d2
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-03-12 23:02:49 +0500

    Time: 3 ms (56.84%) | Memory: 30.3 MB (64.48%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> num(n);
+        int s = nums.size();
+        vector<int> num(s);
         int start = 0;
-        int end =n-1;
-        int pos = n-1;
+        int end = s-1;
+        int pos = s-1;
+
         while(start<=end){
-            int startSqr = nums[start]*nums[start];
-            int endSqr = nums[end]*nums[end];
-            if(startSqr > endSqr){
-                num[pos] = startSqr;
-                start++;
-            }
-            else{
+            int startSqr = nums[start] * nums[start];
+            int endSqr = nums[end] * nums[end];
+            if(startSqr < endSqr){
                 num[pos] = endSqr;
                 end--;
+            }
+            else{
+                num[pos] = startSqr;
+                start++;
             }
             pos--;
         }
