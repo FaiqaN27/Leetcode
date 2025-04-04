ce3a05b41a266a0d1bef5b866f6faa43e510938f
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-04-05 00:50:00 +0500

    Time: 0 ms (100.00%) | Memory: 9.9 MB (23.85%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -3,10 +3,12 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         vector<int> freq(101 , 0);
         int count = 0;
-        for(int num : nums){
-            count += freq[num];
-            freq[num]++;
+        for(int n : nums){
+            count += freq[n];
+
+            freq[n]++;
         }
+
         return count;
     }
 };
