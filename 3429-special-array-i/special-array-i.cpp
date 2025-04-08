b62fe49c732440d54ab1d46323978c6e2151504e
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-04-08 23:15:54 +0500

    Time: 0 ms (100.00%) | Memory: 27.5 MB (99.38%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -3,6 +3,7 @@ public:
     bool isArraySpecial(vector<int>& nums) {
     int i = 0;
     int n = nums.size();
+    if (n<=1) return true;
     while(i<n-1){
         if((nums[i]%2 == 0 && nums[i+1]%2 ==0) ||
          (nums[i]%2 != 0 && nums[i+1]%2 !=0)){
