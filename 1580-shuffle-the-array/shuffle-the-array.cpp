0fe054f5afcda903806a7aa81fa815db89d4f0c0
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-06-20 12:27:23 +0500

    Time: 0 ms (100.00%) | Memory: 13.3 MB (82.42%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -2,15 +2,10 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> res;
-
-       for(int i = 0; i<n; i++){
-
-        res.push_back(nums[i]);
-        res.push_back(nums[i+n]);
-        
-       }
-    
+        for(int i= 0; i<n; i++){
+            res.push_back(nums[i]);
+            res.push_back(nums[i+n]);
+        }
         return res;
     }
-    
 };
