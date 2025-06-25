bd527b8f35919c624fcda6b1fb75106b05bbc635
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-06-26 01:41:24 +0500

    Time: 0 ms (100.00%) | Memory: 9.7 MB (74.07%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,9 +3,16 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
 
-        sort(s.begin() , s.end());
-        sort(t.begin(), t.end());
+        int count[26] = {0};
+        for(int i=0; i<s.size(); i++){
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
 
-        return s==t;
+        for(int i=0; i<26 ; i++){
+            if(count[i] != 0) return false;
+        }
+
+        return true;
     }
 };
