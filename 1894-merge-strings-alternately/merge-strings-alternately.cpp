0ea8f3369e0fe43026e945a870beac4b3edaedd2
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-09-08 00:37:35 +0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (63.20%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     string mergeAlternately(string w1, string w2) {
         string combo = "";
-        int i = 0;
-        int j = 0;
-        while(i<w1.size() || j<w2.size()){
-        
-        if(i<w1.size()){
-            combo.push_back(w1[i]);
-		    i++;
+        int left = 0;
+        int right = 0;
+        while(left<w1.size() && right<w2.size()){
+            combo.push_back(w1[left]);
+            combo.push_back(w2[right]);
+            left++;
+            right++;
         }
-        
-        if(j<w2.size()){
-			combo.push_back(w2[j]);
-            j++;
+
+        while(left<w1.size()){
+            combo.push_back(w1[left]);
+            left++;
         }
-      }
+
+        while(right<w2.size()){
+            combo.push_back(w2[right]);
+            right++;
+        }
+
         return combo;
     }
 };
