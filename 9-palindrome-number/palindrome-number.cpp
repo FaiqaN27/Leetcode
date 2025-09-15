99bc1457ff2fa79f0d0951315b8cb8a69274e95b
Faiqa Nadeem <faiqanadeem6725@gmail.com>
2025-09-16 03:29:09 +0500

    Time: 4 ms (32.48%) | Memory: 8.6 MB (67.36%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-       int num = x;
-       long rev = 0;
-       while(x>0){
-        int rem = x%10;
-        rev = rev*10 + rem;
-        x/= 10; 
-       }
-       return rev == num;
+        int num = x;
+        long rev = 0;
+        while (x > 0) {
+            int rem = x % 10;
+            rev = (rev * 10) + rem;
+            x = x / 10;
+        }
+
+        return rev == num;
     }
 };
